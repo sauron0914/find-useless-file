8bca9f435e3278316fdbf1fe2752229fc6243d1e
chuFang <chuFang@chuFangdeMacBook-Pro.local>
2020-11-12 08:55:33 +0800

    feat: 🎸 bugfix

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,6 +8,29 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 
 var fs__default = /*#__PURE__*/_interopDefaultLegacy(fs$1);
 
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __spreadArrays() {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+}
+
 var includeFile = ['.tsx', '.jsx', '.ts', '.js', '.vue', '.html', '.less'];
 var matchSuffix = function (str) {
     var res = str.match(/\.\w+/g);
@@ -41,20 +64,22 @@ var argvs = process.argv.splice(3).map(function (item) {
 if (argvs.length !== 2) {
     throw new Error('仅支持命令 find-useless-file do filePath1 filePath2');
 }
-var aliasSrc = function (path) { return dealIndexJS(path.replace(aliasReg, '@')); };
+// const aliasSrc = path => dealIndexJS(path.replace(aliasReg, '@'))
 var dealIndexJS = function (path) { return path.replace(/(\/index)?.js(x)?/g, ''); };
 var findUselessFile = function () {
+    console.log('开始查找文件....');
     var componentsPaths = {};
     traverseFile(cwd + argvs[0], function (path) {
         componentsPaths[path] = 0;
     });
     traverseFile(cwd + argvs[1], function (path) {
         var readFileSyncRes = fs.readFileSync(path, 'utf8');
-        var currentPathLevel = path.match(/[\w\/]+\//ig)[0];
-        var fromList = readFileSyncRes.match(/(from ['.@\/\w-]+')/g) || [];
+        var currentPathLevel = path.match(/[@\w\/-]+\//ig)[0];
+        var fromListFrom = readFileSyncRes.match(/(from ['.@\/\w-]+')/g) || [];
+        var fromList = readFileSyncRes.match(/(import ['.@\/\w-]+')/g) || [];
         // 相对路径匹配
-        var matchRes = fromList.map(function (item) {
-            return item.replace("from ", '').replace(/\'/g, '');
+        var matchRes = __spreadArrays(fromListFrom, fromList).map(function (item) {
+            return item.replace("from ", '').replace("import ").replace(/\'/g, '');
         }).filter(function (item) {
             return item.includes('.') | item.includes('@');
         }).map(function (item) {
@@ -65,33 +90,30 @@ var findUselessFile = function () {
                 var levelCount = item.match(/\.\./g);
                 if (levelCount) {
                     var arr = currentPathLevel.split('/');
-                    return arr.splice(0, arr.length - 2).join('/') + item.replace(/\.\./g, '');
+                    return arr.splice(0, arr.length - (levelCount.length + 1)).join('/') + item.replace(/\.\./g, '');
                 }
                 else {
                     return item.replace('./', currentPathLevel);
                 }
             }
         });
-        Object.entries(componentsPaths).reduce(function (pre, _a) {
-            var key = _a[0];
-            if (readFileSyncRes.includes(aliasSrc(key)) || matchRes.includes(dealIndexJS(key))) {
+        Object.keys(componentsPaths).forEach(function (key) {
+            if (matchRes.some(function (item) { return item.includes(dealIndexJS(key)); })) {
                 componentsPaths[key]++;
             }
-            return pre;
-        }, {});
+        });
     });
     var res = Object.entries(componentsPaths).reduce(function (pre, _a) {
         var key = _a[0], value = _a[1];
         if (!value) {
-            pre.push(key);
+            pre.push(key.replace(cwd, ''));
         }
         return pre;
     }, []);
-    console.log('res', res);
     fs.writeFile(cwd + 'find-useless-file.json', JSON.stringify(res, null, '\t'), {}, function (err) {
         if (err)
             console.log(err);
-        console.log('文件创建成功，地址：' + cwd + fileName);
+        console.log('文件查找成功，存放地址：' + cwd + fileName);
         console.log("!!!\u6CE8\u610F\uFF1A\u9ED8\u8BA4\u4F1A\u5728\u5F53\u524D\u76EE\u5F55\u4E0B\u751F\u6210\u4E00\u4E2A" + fileName + "\u6587\u4EF6");
         exec('open ' + cwd + fileName);
     });
--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,13 @@ if(argvs.length !== 2) {
     throw new Error('仅支持命令 find-useless-file do filePath1 filePath2');
 }
 
-const aliasSrc = path => dealIndexJS(path.replace(aliasReg, '@'))
+// const aliasSrc = path => dealIndexJS(path.replace(aliasReg, '@'))
 
 const dealIndexJS = path => path.replace(/(\/index)?.js(x)?/g, '')
 
 const findUselessFile  = ()=> {
+
+    console.log('开始查找文件....')
 
     const componentsPaths = {}
 
@@ -31,16 +33,14 @@ const findUselessFile  = ()=> {
     })
 
     traverseFile(cwd + argvs[1], path => {
-
         const readFileSyncRes = fs.readFileSync(path , 'utf8')
-
-        const currentPathLevel = path.match(/[\w\/]+\//ig)[0]
-
-        const fromList = readFileSyncRes.match(/(from ['.@\/\w-]+')/g) || []
+        const currentPathLevel = path.match(/[@\w\/-]+\//ig)[0]
+        const fromListFrom = readFileSyncRes.match(/(from ['.@\/\w-]+')/g) || []
+        const fromList = readFileSyncRes.match(/(import ['.@\/\w-]+')/g) || []
 
         // 相对路径匹配
-        const matchRes: string[] = fromList.map(item=> {
-            return item.replace("from ", '').replace(/\'/g, '')
+        const matchRes: string[] = [...fromListFrom, ...fromList].map(item=> {
+            return item.replace("from ", '').replace("import ").replace(/\'/g, '')
         }).filter(item=> {
             return item.includes('.') | item.includes('@')
         }).map(item=>{
@@ -50,33 +50,30 @@ const findUselessFile  = ()=> {
                 const levelCount = item.match(/\.\./g)
                 if(levelCount) {
                     const arr = currentPathLevel.split('/')
-                    return arr.splice(0, arr.length - 2).join('/') + item.replace(/\.\./g, '')
+                    return arr.splice(0, arr.length - (levelCount.length+1)).join('/') + item.replace(/\.\./g, '')
                 } else {
                     return item.replace('./', currentPathLevel)
                 }
             }
         })
 
-        Object.entries(componentsPaths).reduce((pre, [key])=> {
-            if(readFileSyncRes.includes(aliasSrc(key)) || matchRes.includes(dealIndexJS(key))) {
+        Object.keys(componentsPaths).forEach((key)=> {
+            if(matchRes.some(item=> item.includes(dealIndexJS(key)))) {
                 componentsPaths[key]++
             }
-            return pre
-        }, {})
+        })
     })
 
     const res = Object.entries(componentsPaths).reduce((pre, [key, value])=> {
         if(!value) {
-            pre.push(key)
+            pre.push(key.replace(cwd, ''))
         }
         return pre
     }, [])
 
-    console.log('res', res)
-
     fs.writeFile(cwd+'find-useless-file.json', JSON.stringify(res, null, '\t'), {} ,function(err){
         if(err) console.log(err)
-        console.log('文件创建成功，地址：' + cwd+fileName);
+        console.log('文件查找成功，存放地址：' + cwd+fileName);
         console.log(`!!!注意：默认会在当前目录下生成一个${fileName}文件`)
         exec( 'open ' + cwd + fileName)
     })
